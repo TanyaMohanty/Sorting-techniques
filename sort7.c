dde3c58ee224a4a2a4b32714c5245e6644a73063
SuchismitaBarik <71986326+SuchismitaBarik@users.noreply.github.com>
2020-10-01 00:05:18 +0530

    Create sort7.c

---
+++ sort7.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+void partition(int*,int,int);
+void swap(int*,int*);
+int main()
+{
+ int *a,n;
+ printf("\nEnter size of the array");
+ scanf("%d",&n);
+ a=(int*)malloc(n*sizeof(int));
+ printf("\nEnter the array elements:");
+ for(int i=0;i<n;i++)
+  scanf("%d",a+i);
+ printf("Input array:\n");
+ for(int i=0;i<n;i++)
+  printf("%d ",*(a+i));
+ partition(a,0,n-1);
+ printf("\nResultant/Output array is:\n");
+ for(int i=0;i<n;i++)
+  printf("%d ",*(a+i));
+ return 0;
+}
+
+void partition(int*a,int low,int high)
+{
+ int i=low-1;
+ for(int j=low;j<=high;j++)
+ {
+  if(*(a+j)<0)
+  {
+   i++;
+   swap(a+i,a+j);
+  }
+ }
+}
+
+void swap(int *x,int *y)
+{
+ int c=*x;
+ *x=*y;
+ *y=c;
+}
